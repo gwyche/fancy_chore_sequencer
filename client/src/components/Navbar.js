6ee062238ed50ea1248e012a1eb12c7d118572ef
Sarah-ZW <earthclaninfo@gmail.com>
2024-07-08 19:53:35 -0400

    test

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,6 +4,7 @@ const NavBar = ()=> {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
+    <p>test</p>
     <a className="navbar-brand" href="#">Crappy Recipes</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
